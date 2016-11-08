6796c7c0e493eddbdc4ca4e5b1872efc248f6d2d
Nils Frantzen <Nils Frantzen@134.61.181.124>
2016-11-08 13:09:42 +0100

    Moved files in example package

---
+++ src/main/java/examples/LeHavre.java
@@ -0,0 +1,150 @@
+package examples;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.ui.swingViewer.ViewPanel;
+import org.graphstream.ui.view.View;
+import org.graphstream.ui.view.Viewer;
+
+/**
+ * Test class for the graphstream framework. The class displays a city with
+ * streets where the speedlimit and lanes are highlighted
+ * 
+ * @author n.frantzen <nils.frantzen@rwth-aachen.de>
+ *
+ */
+public class LeHavre {
+
+	private static final double MAX_SPEED = 130.0;
+	private static final double SIZE_FACTOR_EDGE = 2.5;
+	private static final Path STYLESHEET_PATH = FileSystems.getDefault().getPath("src", "main", "resources", "examples",
+			"LeHavreStyleSheet.css");
+
+	private static String stylesheet;
+
+	public static void main(String args[]) {
+		try {
+			System.out.println(STYLESHEET_PATH.toAbsolutePath());
+			StringBuffer buffer = new StringBuffer();
+
+			for (String line : Files.readAllLines(STYLESHEET_PATH)) {
+				buffer.append(line);
+			}
+			stylesheet = buffer.toString();
+			System.out.println(stylesheet);
+
+			System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
+			new LeHavre();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Creates the graph example "Le Havre" from an dgs input file.
+	 */
+	public LeHavre() {
+		Graph graph = processInput();
+		configureGraphRender(graph);
+		createViewPanel(graph);
+
+	}
+
+	/**
+	 * Creates and displays a panel which contains the given graph.
+	 * 
+	 * @param graph
+	 *            which should be displayed.
+	 */
+	private void createViewPanel(Graph graph) {
+
+		Viewer viewer = graph.display(false); // Creates the window
+		View view = viewer.getDefaultView();
+		((ViewPanel) view).resizeFrame(1600, 800);
+		view.getCamera().setViewPercent(0.5);
+
+		// Im eigenen Panel
+		/*
+		 * // Viewer viewer = new Viewer(graph, //
+		 * Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD); Viewer viewer = new
+		 * Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
+		 * 
+		 * View view = viewer.addDefaultView(false);
+		 * 
+		 * JFrame frame = new JFrame("title");
+		 * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 * frame.add((Component) view);
+		 * 
+		 * frame.setSize(1600, 800);
+		 * 
+		 * frame.setVisible(true);
+		 */
+
+	}
+
+	/**
+	 * Adds some rendering properties.
+	 * 
+	 * @param graph
+	 *            which rendering should be changed.
+	 */
+	private void configureGraphRender(Graph graph) {
+		graph.addAttribute("ui.quality"); //
+		graph.addAttribute("ui.antialias");
+		graph.addAttribute("ui.stylesheet", stylesheet);
+	}
+
+	/**
+	 * Reads the input file and creates the graph.
+	 * 
+	 * @return created {@link Graph} from input file.
+	 */
+	private Graph processInput() {
+		Graph graph = new MultiGraph("Le Havre");
+		String inputFilePath = "src/main/resources/LeHavre.dgs";
+		try {
+			graph.read(inputFilePath);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		addDesignClassesFromInput(graph);
+		return graph;
+	}
+
+	/**
+	 * Reads the attributes from the Input and adds the specific classes from
+	 * the CSS.
+	 * 
+	 * @param graph
+	 *            which contains edges with attributes.
+	 */
+	private void addDesignClassesFromInput(Graph graph) {
+		for (Edge edge : graph.getEachEdge()) {
+			if (edge.hasAttribute("isTollway")) {
+				edge.addAttribute("ui.class", "tollway");
+			} else if (edge.hasAttribute("isTunnel")) {
+				edge.addAttribute("ui.class", "tunnel");
+			} else if (edge.hasAttribute("isBridge")) {
+				edge.addAttribute("ui.class", "bridge");
+			}
+
+			double speedMax = edge.getNumber("speedMax") / MAX_SPEED;
+			edge.setAttribute("ui.color", speedMax);
+
+			double lanes = edge.getNumber("lanes");
+			if (lanes > 1) {
+				lanes *= SIZE_FACTOR_EDGE;
+			}
+			edge.setAttribute("ui.size", lanes);
+		}
+	}
+}
---
+++ src/main/java/examples/RandomTraffic.java
@@ -0,0 +1,116 @@
+package examples;
+
+import java.io.IOException;
+
+import org.graphstream.algorithm.generator.RandomGenerator;
+import org.graphstream.algorithm.randomWalk.RandomWalk;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.MultiGraph;
+
+/**
+ * 
+ * @author n.frantzen <nils.frantzen@rwth-aachen.de>
+ *
+ */
+public class RandomTraffic {
+
+	protected static String styleSheet = "edge {" + "	size: 2px;" + "	fill-color: green, yellow, red;"
+			+ "	fill-mode: dyn-plain;" + "}" + "node {" + "	size: 6px;" + "	fill-color: #444;" + "}";
+
+	/**
+	 * Start program with
+	 * <ul>
+	 * <li>"java -Dsun.java2d.opengl=True" on linux</li>
+	 * <li>"java -Dsun.java2d.directx=True" on windows</li>
+	 * </ul>
+	 * for better performance.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		changeRenderer();
+
+		Graph graph = createRandomGraph(100);
+		graph.addAttribute("ui.stylesheet", styleSheet);
+		graph.addAttribute("ui.quality");
+		graph.addAttribute("ui.antialias");
+		graph.display();
+
+		System.out.println(graph.getNodeCount());
+		System.out.println(graph.getEdgeCount());
+
+		RandomWalk rWalk;
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		rWalk = new RandomWalk();
+		rWalk.setEntityCount(graph.getNodeCount());
+		rWalk.setEvaporation(0.99);
+		rWalk.setEntityMemory(graph.getNodeCount() / 10);
+		rWalk.init(graph);
+		for (int i = 0; i < 30000; i++) {
+			rWalk.compute();
+			if (i % 10 == 0) {
+				System.out.println("Step: " + i);
+				UpdateGraph(graph, rWalk);
+			}
+
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		rWalk.terminate();
+		System.out.println("done");
+		UpdateGraph(graph, rWalk);
+
+	}
+
+	private static Graph createRandomGraph(int nodeCount) {
+		Graph graph = new MultiGraph("random");
+		RandomGenerator rgen = new RandomGenerator(2.5);
+
+		rgen.addSink(graph);
+		rgen.begin();
+		for (int i = 0; i < nodeCount; i++) {
+			rgen.nextEvents();
+		}
+		rgen.end();
+
+		for (Node node : graph.getEachNode()) {
+			if (node.getEdgeSet().isEmpty()) {
+				graph.removeNode(node);
+			}
+		}
+		return graph;
+
+	}
+
+	private static void UpdateGraph(Graph graph, RandomWalk rWalk) {
+
+		for (Edge edge : graph.getEachEdge()) {
+			double passes = rWalk.getPasses(edge);
+			double color = passes / (graph.getEdgeCount());
+			System.out.println(passes + " = " + color);
+			edge.setAttribute("ui.color", color);
+		}
+	}
+
+	/**
+	 * Changes the graph renderer from default to the advanced viewer, which
+	 * contains an automatic layout algorithm that will try to place the nodes
+	 * so as to make the graph readable by adding a force to every node which
+	 * repels all other nodes and attract those who are connected.
+	 */
+	public static void changeRenderer() {
+		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+		// org.graphstream.ui.swingViewer.GraphRenderer to use own renderer
+
+	}
+
+}
